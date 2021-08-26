1f963ac03945d77bf95f7dd345b8d37f95cd5e24
Marek Mościchowski <marek.moscichowski@miquido.com>
2021-08-26 08:27:12 +0200

    x

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
-//resource "aws_acm_certificate" "default" {
+resource "aws_acm_certificate" "default" {
 //  provider = aws.acm
-//
-//  count                     = local.enabled ? 1 : 0
-//  domain_name               = var.domain_name
-//  validation_method         = var.validation_method
-//  subject_alternative_names = var.subject_alternative_names
-//  tags                      = var.tags
-//
-//  lifecycle {
-//    create_before_destroy = true
-//  }
-//}
+
+  count                     = local.enabled ? 1 : 0
+  domain_name               = var.domain_name
+  validation_method         = var.validation_method
+  subject_alternative_names = var.subject_alternative_names
+  tags                      = var.tags
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
 locals {
   enabled                           = true
@@ -19,8 +19,6 @@ locals {
 }
 
 resource "aws_route53_record" "default" {
-  provider = aws.dns
-
   count           = local.process_domain_validation_options ? length(var.subject_alternative_names) + 1 : 0
   zone_id         = var.hosted_zone_id
   ttl             = var.ttl
@@ -30,10 +28,10 @@ resource "aws_route53_record" "default" {
   records         = [lookup(local.domain_validation_options_list[count.index], "resource_record_value")]
 }
 
-//resource "aws_acm_certificate_validation" "default" {
+resource "aws_acm_certificate_validation" "default" {
 //  provider = aws.acm
-//
-//  count                   = local.process_domain_validation_options && var.wait_for_certificate_issued ? 1 : 0
-//  certificate_arn         = join("", aws_acm_certificate.default.*.arn)
-//  validation_record_fqdns = aws_route53_record.default.*.fqdn
-//}
+
+  count                   = local.process_domain_validation_options && var.wait_for_certificate_issued ? 1 : 0
+  certificate_arn         = join("", aws_acm_certificate.default.*.arn)
+  validation_record_fqdns = aws_route53_record.default.*.fqdn
+}
