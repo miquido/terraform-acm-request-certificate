55a7bc41b06f56738b1919bd62c66679a3be82e4
Miquido Ops <mqtc@miquido.com>
2022-03-25 11:19:38 +0000

    chore(deps): update hashicorp/aws requirement from ~> 3.0 to ~> 4.7

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source                = "hashicorp/aws"
-      version               = "~> 3.0"
+      version               = "~> 4.7"
       configuration_aliases = [aws.acm, aws.dns]
     }
     local = {
